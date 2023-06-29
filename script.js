8101396e811df9fd39546429029b65f4e2d5c1d6
raymondcruzcabrera <138133652+raymondcruzcabrera@users.noreply.github.com>
2023-06-29 09:02:48 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+var firstname = "spooder";
+var lastname = "pooper";
+var numberOfsiblings = 2;
+haspets = (true);
+
+console.log(firstname);
+console.log(lastname);
+console.log(numberOfsiblings);
+
+
+
+var button = document.getElementById("hi");
+var h1 = document.getElementById("haha yes");
+var body = document.body;
+
+button.addEventListener("click", function(){
+    console.log("click");
+
+});
+
+
+button.addEventListener("click", function)(){}
+haha yes.innerHTML = "hamburger"
